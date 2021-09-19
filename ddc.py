8cb6d579a39c18541a3f549d8f87fdcc210ad779
Jake <git@ja-ke.tech>
2021-09-19 17:05:20 +0200

    Delete  SWIG and python lib experiments decided for ddcutil/CFFI because it just works closes #2

--- ddc.py
+++
@@ -1,5 +0,0 @@
-import ddc, sys
-
-i2c_id = int(sys.argv[1])
-dev = ddc.DDCCIDevice(i2c_id)
-print(dev.brightness)
--- swig/main.py
+++
@@ -1,8 +0,0 @@
-import ddcutil
-import _ddcutil as cddc
-
-if __name__ == "__main__":
-    out = ddcutil.DDCA_Display_Info_List()
-    ret = ddcutil.ddca_get_display_info_list2(True, out)
-    print(ret)
-    print(out)
